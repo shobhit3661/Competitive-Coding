e522fcb403f767116e3185d92a93aaf73d0b0252
shobhit sharma <shobhitsharma.3661@gmail.com>
2020-11-30 20:32:52 +0530

    11/29/2020

---
+++ Codeforces/1455A.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define fast  ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define arin(x,n) for(int i=0;i<n;i++)cin>>x[i]
+#define debug(x,n) for(int i=0;i<n;i++)cout<<x[i]<<" "
+#define pb push_back
+typedef long long int ll;
+
+using namespace std;
+
+void solve()
+{
+	string s;
+	cin>>s;
+	
+	cout<<s.length()<<endl;
+}
+
+int main()
+{
+	fast;
+	ll t=1;
+	cin>>t;
+	while(t--)
+		solve();
+	return 0;
+}
+
+
+

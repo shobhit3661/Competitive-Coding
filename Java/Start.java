b9e2210b432ef26de251fa72104dff8d5ad415d7
shobhit sharma <shobhitsharma.3661@gmail.com>
2021-03-15 17:56:07 +0530

    3/15/2021

--- Java/Start.java
+++ Java/Start.java
@@ -1,7 +1,19 @@
-class Start
-{
-    public static void main(String[] args) 
-    {
-        System.out.println("shobhit");
-    }
+import java.util.*;
+public class Start{
+    public static void main(String[] args){
+ Scanner sc = new Scanner(System.in);
+ int noOfItems = sc.nextInt();
+ String input="";
+ int price[] = new int[noOfItems];
+ int percentage[] = new int[noOfItems];
+ String itemName[] = new String[noOfItems];
+ String[] values = new String[3];
+ for(int i=0;i<noOfItems;++i){
+  input = sc.nextLine();
+  values = input.split(",");
+  itemName[i] = values[0];
+  price[i] = Integer.parseInt(values[1]);
+  percentage[i] = Integer.parseInt(values[2]);
+ }
+}
 }
